9947a5591f037f353d27f84e024f8abd05729050
ergoz <ergozru@riftbit.com>
2018-10-23 16:42:43 +0300

    add additional error types and ready to use messages

--- errors.go
+++ errors.go
@@ -9,16 +9,76 @@ const (
 	// ParseError Parse error - Invalid JSON was received by the server.
 	// An error occurred on the server while parsing the JSON text.
 	ParseError ErrorCode = -32700
+
+	// ParseErrorMessage Parse error - Invalid JSON was received by the server.
+	// An error occurred on the server while parsing the JSON text.
+	ParseErrorMessage = "Parse error"
+
 	// InvalidRequestError Invalid Request - The JSON sent is not a valid Request object.
 	InvalidRequestError ErrorCode = -32600
+
+	// InvalidRequestMessage Invalid Request - The JSON sent is not a valid Request object.
+	InvalidRequestMessage = "Invalid request"
+
 	// MethodNotFoundError Method not found - The method does not exist / is not available.
 	MethodNotFoundError ErrorCode = -32601
+
+	// MethodNotFoundMessage Method not found - The method does not exist / is not available.
+	MethodNotFoundMessage = "Method not found"
+
 	// InvalidParamsError Invalid params - Invalid method parameter(s).
 	InvalidParamsError ErrorCode = -32602
+
+	// InvalidParamsMessage Invalid params - Invalid method parameter(s).
+	InvalidParamsMessage = "Invalid params"
+
 	// InternalError Internal error - Internal JSON-RPC error.
 	InternalError ErrorCode = -32603
+
+	// InternalErrorMessage Internal error - Internal JSON-RPC error.
+	InternalErrorMessage = "Internal error"
+
 	// ServerError Server error - Reserved for implementation-defined server-errors.
 	ServerError ErrorCode = -32000
+
+	// ServerErrorMessage Server error - Reserved for implementation-defined server-errors.
+	ServerErrorMessage = "Server error"
+
+	// DataNotFoundError ...
+	DataNotFoundError ErrorCode = -32001
+
+	// DataNotFoundTitle ...
+	DataNotFoundTitle = "Data not found"
+
+	// PermissionDeniedError ...
+	PermissionDeniedError ErrorCode = -32602
+
+	// PermissionDeniedMessage ...
+	PermissionDeniedMessage = "Permission denied"
+
+	// AlreadyExecutedError ...
+	AlreadyExecutedError ErrorCode = -32003
+
+	// AlreadyExecutedMessage ...
+	AlreadyExecutedMessage = "Already executed"
+
+	// RegistrationIsRequiredError ...
+	RegistrationIsRequiredError ErrorCode = -32004
+
+	// RegistrationIsRequiredMessage ...
+	RegistrationIsRequiredMessage = "Registration is required"
+
+	// CheckIsRequiredError ...
+	CheckIsRequiredError ErrorCode = -32005
+
+	// CheckIsRequiredMessage ...
+	CheckIsRequiredMessage = "Check is required"
+
+	// ExternalServiceError ...
+	ExternalServiceError ErrorCode = -32006
+
+	// ExternalServiceMessage ...
+	ExternalServiceMessage = "Error occurred when calling external service, try again later"
 )
 
 // ErrNullResult null result error
